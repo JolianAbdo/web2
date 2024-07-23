e374adfbeafc91516879b3ea64e59e8d91546e79
leen7awa <leen.hawa@e.braude.ac.il>
2024-07-23 23:56:00 +0300

    added register window
    *added register window
    *added new component to switch windows

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,8 +2,7 @@ import { render } from "react";
 import { useState } from "preact/hooks";
 // import './index.css'
 import HeroSection from './HeroSection';
-
-
+import SwitchScreen from "../components/LoginOrRegister";
 
 
 const Body = () => {
@@ -42,13 +41,17 @@ const Body = () => {
         const buttonId = event.currentTarget.id;
         switch (buttonId) {
             case "join-btn":
+                console.log(activeTab);
                 setActiveTab("login");
                 break;
             case "back-btn":
                 setActiveTab("home");
                 break;
-            // add login button and check fields etc..
+            case "register-btn":
+                setActiveTab("register");
+                break;
             default:
+                console.log(activeTab);
                 break;
         }
     };
@@ -74,32 +77,44 @@ const Body = () => {
     const renderLoginScreen = () => {
         return (
             <>
-                <div className="inset-0 mx-auto container bg-slate-100 flex flex-col gap-8 rounded-2xl w-1/3 h-80 justify-center items-center p-12">
-                    <div className="flex flex-col items-center gap-2">
-                        <div className="font-bold text-black text-2xl">Login</div>
-                        <div>
-                            <label htmlFor="name" className="block text-sm font-medium text-black">Name</label>
-                            <input type="text" id="name" name="name" className="mt-1 block w-80 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-8 mg-10 pl-2" />
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="block text-sm font-medium text-black">Password</label>
-                            <input type="password" id="password" name="password" className="mt-1 block w-80 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-8 pl-2" />
-                        </div>
-                        <button type="submit" className="w-64 bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition">Login</button>
-                        <button
-                            id="back-btn"
-                            className="bg-slate-400 w-40 text-white p-1 rounded-lg hover:bg-slate-600"
-                            onClick={handleJoinClick}
-                        >
-                            Back
-                        </button>
-                        <p id="switch-auth" class="text-center text-blue-600 mt-0 cursor-pointer">
-                            Don't have an account? <span class="underline">Register here</span>
-                        </p>
-                    </div>
-                </div>
+                <SwitchScreen
+                    title="Login"
+                    buttonText="Login"
+                    btnName="register-btn"
+                    alternateText="Don't have an account?"
+                    alternateActionText="Register here"
+                    handleButtonClick={handleJoinClick}
+                />
             </>
         )
+    }
+
+    const renderRegisterScreen = () => {
+        return (
+            <>
+                <SwitchScreen
+                    title="Register"
+                    buttonText="Register"
+                    btnName="join-btn"
+                    alternateText="Already have an account?"
+                    alternateActionText="Login here"
+                    handleButtonClick={handleJoinClick}
+                />
+            </>
+        )
+    }
+
+    const renderpages = () => {
+        switch (activeTab) {
+            case "home":
+                return renderHomeBody();
+            case "login":
+                return renderLoginScreen();
+            case "register":
+                return renderRegisterScreen();
+            default:
+                break;
+        }
     }
 
     return (
@@ -110,7 +125,7 @@ const Body = () => {
                 className="h-screen bg-cover bg-center flex justify-center items-center w-full"
                 style={{ backgroundImage: `url(${preloadedImages[0].src})` }}
             >
-                {activeTab == "home" ? renderHomeBody() : renderLoginScreen()}
+                {renderpages()}
             </div>
         </>
     );
