11af72cd0c66abb93c00421e4a1316eb847a8b71
Jolian <jolianabdo@gmail.com>
2024-07-28 20:26:46 +0300

    update vercel 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
